114dcfed87925ba0bff04cb61c26ea6afc01080e
subodh <bajrasubodh@gmail.com>
2019-04-25 18:44:53 -0500

    changed

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello {
 	public static void main (String[]args) {
 		for(int i=1; i<=10; i++){
 			System.out.println("hello world" +i);
+			System.out.println("how are you doing");
 		}
 	}
 }
