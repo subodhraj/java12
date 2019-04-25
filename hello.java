c164a81116d432a19a1b19adc7a4981eac08f823
subodh <bajrasubodh@gmail.com>
2019-04-25 18:26:03 -0500

    hello.java added

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-public class new {
-	public static void main(String[]args){
-		for(int i=1;i<=10;i++){
+public class hello {
+	public static void main (String[]args) {
+		for(int i=1; i<=10; i++){
 			System.out.println("hello world" +i);
 		}
 	}
